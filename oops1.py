24025c02d845fbfb9a492b1577a6eda3b5d613a0
Agniva <agnivac11@gmail.com>
2025-02-12 00:57:01 +0530

    Changes

--- oops1.py
+++ oops1.py
@@ -1,17 +1,20 @@
-class Employee:
-    # Assigning attributes using constructor
-    def __init__(self, id, salary, designation):
-        self.id = id
-        self.salary = salary
-        self.designation = designation
+# class Employee:
+#     # Assigning attributes using constructor
+#     def __init__(self, id, salary, designation):
+#         self.id = id
+#         self.salary = salary
+#         self.designation = designation
         
-    def travel(self, destination):
-        print(f"Employee is travelling to {destination}")
+#     def travel(self, destination):
+#         print(f"Employee is travelling to {destination}")
         
-# creating objects
-Sam = Employee(123, 25000, "MLE")
-# print(Sam.id)
-# print(Sam.salary)
-# print(Sam.designation)
+# # creating objects
+# Sam = Employee(123, 25000, "MLE")
+# # print(Sam.id)
+# # print(Sam.salary)
+# # print(Sam.designation)
     
-Sam.travel("Kerala")
+# Sam.travel("Kerala")
+
+from oops_project import chatbook
+user = chatbook()
