9885c6d65558bdcc3d24009cc875a9dd103f8036
Agniva <agnivac11@gmail.com>
2025-02-11 23:18:10 +0530

    Second commit

---
+++ oops1.py
@@ -0,0 +1,17 @@
+class Employee:
+    # Assigning attributes using constructor
+    def __init__(self, id, salary, designation):
+        self.id = id
+        self.salary = salary
+        self.designation = designation
+        
+    def travel(self, destination):
+        print(f"Employee is travelling to {destination}")
+        
+# creating objects
+Sam = Employee(123, 25000, "MLE")
+# print(Sam.id)
+# print(Sam.salary)
+# print(Sam.designation)
+    
+Sam.travel("Kerala")
